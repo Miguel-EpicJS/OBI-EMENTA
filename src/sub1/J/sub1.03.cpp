2c8323e7047d2f1e8a80b36d6f36896862142d20
CodigoDeSenior <migueljnm840@gmail.com>
2023-02-12 16:02:07 -0300

    [J] Linha, segmento de linha, ângulo, triângulo, retângulo, quadrado, circunferência.

---
+++ src/sub1/J/sub1.03.cpp
@@ -0,0 +1,5 @@
+// Conceitos básicos de Aritmética e Geometria
+
+// [J] Linha, segmento de linha, ângulo, triângulo, retângulo, quadrado, circunferência.
+
+// https://escolakids.uol.com.br/matematica/geometria-plana.htm
