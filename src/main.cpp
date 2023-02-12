bbc1b0598cf3e07d61e8e1b571c9bc32af8010fc
CodigoDeSenior <migueljnm840@gmail.com>
2023-02-12 15:26:08 -0300

    initial menu

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "BEM-VIM AO GERENCIADOR DE ALGORITMOS PARA A OBI\n\n";
+
+    cout << "SELECIONE A CATEGORIA QUE DESEJA EXPLORAR:\n";
+
+    cout << "(0) ARITMÉTICA E GEOMETRIA BÁSICA\n";
+    cout << "(1) MATEMÁTICA DISCRETA\n";
+    cout << "(2) GRAFOS\n";
+    cout << "(3) ESTRUTURA DE DADOS\n";
+    cout << "(4) GEOMETRIA COMPUTACIONAL\n\n";
+    cout << "(>4) EXIT\n\n";
+
+    int option;
+
+    cin >> option;
+
+    switch (option)
+    {
+	case 0:
+	    break;
+	case 1:
+	    break;
+	case 2:
+	    break;
+	case 3:
+	    break;
+	case 4:
+	    break;
+	default:
+	    return 0;
+	    break;
+    }
+
+    return 0;
+}
+
