f379b1190686ce194cc77ff8db50bc27e8864f30
CodigoDeSenior <migueljnm840@gmail.com>
2023-02-12 16:00:41 -0300

    [J] Frações.

---
+++ src/sub1/J/sub1.02.cpp
@@ -0,0 +1,25 @@
+// Conceitos básicos de Aritmética e Geometria
+
+// [J] Frações. 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n1, n2; 
+
+    n1 = 100; 
+    n2 = 75;
+
+    cout << "n1 / n2 = "  << n1 / n2 << "\n";
+    cout << "n2 / n1 = "  << n2 / n1 << "\n";
+    cout << "n1 / n2 = "  << (float) n1 / n2 << "\n";
+    cout << "n1 / n2 = "  << n1 / 75.0 << "\n";
+    cout << "n2 / n1 = "  << 75.f / n1 << "\n";
+
+
+    return 0;
+}
