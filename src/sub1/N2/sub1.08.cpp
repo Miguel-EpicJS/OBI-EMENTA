4a7401f0f2c6b388c3a40f0582d31cb6a3e3c4c5
CodigoDeSenior <migueljnm840@gmail.com>
2023-02-18 08:04:39 -0300

    new ex

---
+++ src/sub1/N2/sub1.08.cpp
@@ -0,0 +1,15 @@
+// Conceitos básicos de Aritmética e Geometria
+
+// [N2] Aritmética modular básica: adição, subtração e multiplicação.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
+
